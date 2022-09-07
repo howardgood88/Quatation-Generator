203e82152711a34180741034bfa4a71eb20b7376
howardgood88 <howardgood88@gmail.com>
2022-09-07 22:46:06 +0800

    Test file

--- quotation.py
+++ quotation.py
@@ -1,8 +1,14 @@
 from numbers_parser import Document
 
-doc = Document("test.numbers")
+doc = Document("test(0.75).numbers")
 sheets = doc.sheets
 tables = sheets[0].tables
 table = tables[0]
-for row in table.iter_rows():
-    print(row.value)
+for row in table.iter_rows(values_only = True):
+    print(row)
+table.write("B8", "Fuck you")
+table.write("C9", "bitch")
+print()
+for row in table.iter_rows(values_only = True):
+    print(row)
+doc.save("test_new.numbers")
